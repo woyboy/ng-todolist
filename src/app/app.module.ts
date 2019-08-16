db30d9398900dce1cfe74f9bb6a85b4fb45a3f6b
woyboy <mtwoytovich@gmail.com>
2019-08-16 07:55:50 -0500

    Added media query to allow for responsivenessfor mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { DataEntryComponent } from './data-entry/data-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataEntryComponent
   ],
   imports: [
     BrowserModule
