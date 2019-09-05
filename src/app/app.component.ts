5fb8f31e29db984d57584337be45d8cba5508d76
woyboy <mtwoytovich@gmail.com>
2019-09-05 06:54:29 -0400

    Add mobile friendliness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,38 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LET\'S DO THIS SHIT!';
+  fullTitle = 'LET\'S DO THIS SHIT!';
+  abbTitle = 'LDTS!';
   intro = 'Keep track of all the shit you are going to do today!';
   step1 = 'Enter new shit using the field below.';
   step2 = 'Then mark shit off when you finish it.';
   taskList: {value: string, complete: boolean}[] = [];
+  newTask: any;
+  newTaskValue: any;
+  time: any;
 
-clearInput(): void {
-  const clearText = document.querySelector('#new-task') as HTMLInputElement;
-  clearText.value = '';
-}
 
-addNewTask(): void {
-  const newTask = document.querySelector('#new-task') as HTMLInputElement;
-  const newTaskValue = newTask.value;
-  this.taskList.push({value: newTaskValue, complete: false});
-  this.clearInput();
-}
+  addNewTask(): void {
+    this.newTask = document.querySelector('#new-task') as HTMLInputElement;
+    this.newTaskValue = this.newTask.value;
+    this.taskList.push({value: this.newTaskValue, complete: false});
+    this.clearInput();
 
-taskIsDone(index: number): void {
-  this.taskList[index].complete = !this.taskList[index].complete;
-}
-
-clearTasks(startingElement: number, numberOfElements: number): void {
-  this.taskList.splice(startingElement, numberOfElements);
-}
-
-printToConsole() {
-  let i = 0;
-  for (i; i < this.taskList.length; i++) {
-    console.log(this.taskList[i]);
   }
-}
+
+  clearInput(): void {
+    const clearText = document.querySelector('#new-task') as HTMLInputElement;
+    clearText.value = '';
+  }
+
+  taskIsDone(index: number): void {
+    this.taskList[index].complete = !this.taskList[index].complete;
+  }
+
+  clearTasks(startingElement: number, numberOfElements: number): void {
+    this.taskList.splice(startingElement, numberOfElements);
+  }
+
+  deleteTask(index: number) {
+    const taskToDelete: number = index;
+    this.time = setTimeout(() => {
+      this.taskList.splice(taskToDelete, 1);
+    }, 1000);
+  }
+
+  clearTime() {
+    clearTimeout(this.time);
+  }
+
+  printToConsole() {
+    let i = 0;
+    for (i; i < this.taskList.length; i++) {
+      console.log(this.taskList[i]);
+    }
+  }
+
 }
 
 
