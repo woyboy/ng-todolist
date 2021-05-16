2de914cbb2ec173da70d7283057ae89086f8c0b8
woyboy <mtwoytovich@gmail.com>
2021-05-16 09:59:31 -0400

    Remove data entry component which was not being used.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { DataEntryComponent } from './data-entry/data-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataEntryComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
